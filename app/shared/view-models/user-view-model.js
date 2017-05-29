dfbd596cb0459053b3bcca3f38204e2e0b8699dc
CCKRK <codykessler@gmail.com>
2017-05-29 00:49:39 -0500

    refactoring calls to remote API login completed successfully

--- app/shared/view-models/user-view-model.js
+++ app/shared/view-models/user-view-model.js
@@ -2,6 +2,7 @@ var config = require("../../shared/config");
 var fetchModule = require("fetch");
 var appSettings = require('application-settings')
 var validator = require("email-validator");
+var http = require("http");
 //var Https =require("nativescript-https");
 // god this needs https
 var observableModule = require("data/observable");
@@ -12,7 +13,28 @@ function User(info) {
         email: info.email || "",
         password: info.password || ""
     });
-    viewModel.login = function() {
+    var result;
+    viewModel.login = function(){
+     return http.request({
+        //url: "http://cckrk.pythonanywhere.com/AuthenticateUser",
+        url: config.apiURL + '/AuthenticateUser',
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        content: JSON.stringify({ email: viewModel.get("email"), password: viewModel.get("password") })
+    })
+    //.then(handleErrors)
+    .then(function (response) {
+        //console.log(JSON.stringify(response.content));
+        return  response.content.toJSON();
+
+        }).then(function(data){
+            if(data.status != 200){
+                throw Error(data.status);
+            }
+            return data;//console.log(data.UserID);
+        })
+    };
+    /*viewModel.login = function() {
         return fetchModule.fetch(config.apiUrl + "/AuthenticateUser", {
             method: "POST",
             body: JSON.stringify({
@@ -38,7 +60,7 @@ function User(info) {
             //TODO: needs auth support
             appSettings.setString('token',data.UserID);
         })
-    };
+    };*/
     viewModel.register = function() {
         return fetchModule.fetch(config.apiUrl + "/CreateUser", {
             method: "POST",
@@ -68,9 +90,9 @@ function User(info) {
     return viewModel;
 }
 function handleErrors(response) {
-    if (!response.ok) {
-        console.log(JSON.stringify(response));
-        throw Error(response.statusText);
+    if (response.content.status != 200) {
+        console.log(JSON.stringify(response.content.status));
+        throw Error(response.content.status);
     }
     return response;
 }
