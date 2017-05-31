7728115bc8cbea0494f5ca08d85187eae9ca4754
CCKRK <codykessler@gmail.com>
2017-05-30 22:26:32 -0500

    FontIcons working correctly, would be nice to have highlighting.

--- platforms/ios/workoutapp/app/views/routines/routines.js
+++ platforms/ios/workoutapp/app/views/routines/routines.js
@@ -13,14 +13,10 @@ var drawer;
 var view = require("ui/core/view");
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// >> sidedrawer-getting-started-binding-context
-var getting_started_model_1 = require("../../shared/view-models/routine-view-model");
 function pageLoaded(args) {
     var page = args.object;
     drawer = view.getViewById(page, "sideDrawer");
-    var page2 = args.object;
-    page.bindingContext = new getting_started_model_1.GettingStartedViewModel();
-    page2.bindingContext = pageData;
+    page.bindingContext = pageData;
     var listView = page.getViewById("routineList");
     routineList.empty();
     pageData.set("isLoading", true);
